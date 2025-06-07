ce00b037c886359e712a5a8e2d8b4da49a8f331e
james <soapy.dev@proton.me>
2025-06-06 21:53:55 -0700

    dll fix

--- include/sf/export.h
+++ include/sf/export.h
@@ -2,10 +2,14 @@
 #define EXPORT_H
 
 #ifdef _WIN32
-#    ifdef SF_EXPORTS
-#        define EXPORT __declspec(dllexport)
+#    ifdef SF_DYNAMIC
+#        ifdef SF_EXPORTS
+#            define EXPORT __declspec(dllexport)
+#        else
+#            define EXPORT __declspec(dllimport)
+#        endif
 #    else
-#        define EXPORT __declspec(dllimport)
+#        define EXPORT
 #    endif
 #elif
 #    define EXPORT
