cfd0a65f74b96c02ddd6f111322f2b62536b0c68
james <soapy.dev@proton.me>
2025-06-09 20:27:51 -0700

    files

---
+++ include/sf/files.h
@@ -0,0 +1,12 @@
+#ifndef FILES_H
+#define FILES_H
+
+#include "sf/result.h"
+
+/// Get the size of a file at the specified path. Returns -1 if the file doesn't exist.
+long sf_file_size(sf_str path);
+#define sf_file_exists(path) (sf_file_size(path) >= 0)
+/// Load a file into a byte buffer. Returns an error result on failure.
+[[nodiscard]] sf_result sf_load_file(uint8_t *out, sf_str path);
+
+#endif // FILES_H
---
+++ src/files.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include "sf/files.h"
+
+long sf_file_size(sf_str path) {
+    struct stat s;
+    if (stat(path.c_str, &s) == -1)
+        return -1;
+    return s.st_size;
+}
+
+sf_result sf_load_file(uint8_t *out, sf_str path) {
+    long size = sf_file_size(path);
+    if (size < 0)
+        return sf_err(sf_str_fmt("Requested file '%s' could not be found.", path));
+    FILE *f = fopen(path.c_str, "rb");
+    if (!f)
+        return sf_err(sf_str_fmt("Call to fopen on requested file '%s' failed.", path));
+
+    // Read into buffer
+    if (fread(out, (unsigned long long)size, 1, f) < 0) {
+        fclose(f);
+        return sf_err(sf_str_fmt("Requested file '%s' could not be read.", path));
+    }
+    fclose(f);
+
+    return sf_ok();
+}
