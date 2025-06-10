aec678986631bb9b825b07985b10733cdf10b626
james <soapy.dev@proton.me>
2025-06-10 12:52:23 -0700

    Update fs.h

--- include/sf/fs.h
+++ include/sf/fs.h
@@ -4,9 +4,9 @@
 #include "sf/result.h"
 
 /// Get the size of a file at the specified path. Returns -1 if the file doesn't exist.
-long sf_file_size(sf_str path);
+EXPORT long sf_file_size(sf_str path);
 #define sf_file_exists(path) (sf_file_size(path) >= 0)
 /// Load a file into a byte buffer. Returns an error result on failure.
-[[nodiscard]] sf_result sf_load_file(uint8_t *out, sf_str path);
+[[nodiscard]] EXPORT sf_result sf_load_file(uint8_t *out, sf_str path);
 
 #endif // FILES_H
