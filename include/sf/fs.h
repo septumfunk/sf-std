50893e97102e29e309c604196cdac7da6aefd37a
septumfunk <jfleischman6@gmail.com>
2025-11-23 01:58:43 -0800

    no more results >:3

--- include/sf/fs.h
+++ include/sf/fs.h
@@ -1,12 +1,23 @@
 #ifndef FILES_H
 #define FILES_H
 
-#include "sf/result.h"
+#include "sf/str.h"
+#include "export.h"
+
+typedef enum {
+    SF_FILE_NOT_FOUND,
+    SF_OPEN_FAILURE,
+    SF_READ_FAILURE,
+} sf_fs_err;
+
+#define EXPECTED_NAME sf_fs_ex
+#define EXPECTED_E sf_fs_err
+#include "sf/containers/expected.h"
 
 /// Get the size of a file at the specified path. Returns -1 if the file doesn't exist.
 EXPORT long sf_file_size(sf_str path);
 #define sf_file_exists(path) (sf_file_size(path) >= 0)
 /// Load a file into a byte buffer. Returns an error result on failure.
-EXPORT sf_result sf_load_file(uint8_t *out, sf_str path);
+EXPORT sf_fs_ex sf_load_file(uint8_t *out, sf_str path);
 
 #endif // FILES_H
--- src/fs.c
+++ src/fs.c
@@ -9,20 +9,20 @@ long sf_file_size(const sf_str path) {
     return s.st_size;
 }
 
-sf_result sf_load_file(uint8_t *out, const sf_str path) {
+sf_fs_ex sf_load_file(uint8_t *out, const sf_str path) {
     const long size = sf_file_size(path);
     if (size < 0)
-        return sf_err(sf_str_fmt("Requested file '%s' could not be found.", path));
+        return sf_fs_ex_err(SF_FILE_NOT_FOUND);
     FILE *f = fopen(path.c_str, "rb");
     if (!f)
-        return sf_err(sf_str_fmt("Call to fopen on requested file '%s' failed.", path));
+        return sf_fs_ex_err(SF_OPEN_FAILURE);
 
     // Read into buffer
     if (fread(out, (unsigned long long)size, 1, f) < 0) {
         fclose(f);
-        return sf_err(sf_str_fmt("Requested file '%s' could not be read.", path));
+        return sf_fs_ex_err(SF_READ_FAILURE);
     }
     fclose(f);
 
-    return sf_ok();
+    return sf_fs_ex_ok();
 }
