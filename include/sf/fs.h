ac47a57382ba46a39e1ad386140aad5feb2b1541
septumfunk <jfleischman6@gmail.com>
2025-12-03 22:42:19 -0800

    sf_file_buffer

--- include/sf/fs.h
+++ include/sf/fs.h
@@ -1,6 +1,7 @@
 #ifndef FILES_H
 #define FILES_H
 
+#include "sf/containers/buffer.h"
 #include "sf/str.h"
 #include "export.h"
 
@@ -14,10 +15,17 @@ typedef enum {
 #define EXPECTED_E sf_fs_err
 #include "sf/containers/expected.h"
 
+#define EXPECTED_NAME sf_fsb_ex
+#define EXPECTED_O sf_buffer
+#define EXPECTED_E sf_fs_err
+#include "sf/containers/expected.h"
+
 /// Get the size of a file at the specified path. Returns -1 if the file doesn't exist.
 EXPORT long sf_file_size(sf_str path);
 #define sf_file_exists(path) (sf_file_size(path) >= 0)
-/// Load a file into a byte buffer. Returns an error result on failure.
+/// Load a file into a preallocated buffer. Returns an error result on failure.
 EXPORT sf_fs_ex sf_load_file(uint8_t *out, sf_str path);
+/// Load a file as an sf_buffer.
+EXPORT sf_fsb_ex sf_file_buffer(sf_str path);
 
 #endif // FILES_H
--- src/fs.c
+++ src/fs.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include "sf/fs.h"
+#include "sf/containers/buffer.h"
 
 long sf_file_size(const sf_str path) {
     struct stat s;
@@ -25,4 +26,25 @@ sf_fs_ex sf_load_file(uint8_t *out, const sf_str path) {
     fclose(f);
 
     return sf_fs_ex_ok();
+}
+
+sf_fsb_ex sf_file_buffer(sf_str path) {
+    const size_t size = (size_t)sf_file_size(path);
+    if (size < 0)
+        return sf_fsb_ex_err(SF_FILE_NOT_FOUND);
+    FILE *f = fopen(path.c_str, "rb");
+    if (!f)
+        return sf_fsb_ex_err(SF_OPEN_FAILURE);
+    sf_buffer out = sf_buffer_fixed(size);
+    out.flags = SF_BUFFER_GROW;
+
+    // Read into buffer
+    if (fread(out.ptr, size, 1, f) < 0) {
+        fclose(f);
+        sf_buffer_clear(&out);
+        return sf_fsb_ex_err(SF_READ_FAILURE);
+    }
+    fclose(f);
+
+    return sf_fsb_ex_ok(out);
 }
