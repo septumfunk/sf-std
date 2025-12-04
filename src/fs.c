49af5810094cf98f140de852b44453a4b5f19f7c
septumfunk <jfleischman6@gmail.com>
2025-12-03 22:52:00 -0800

    Update fs.c

--- src/fs.c
+++ src/fs.c
@@ -36,6 +36,7 @@ sf_fsb_ex sf_file_buffer(sf_str path) {
     if (!f)
         return sf_fsb_ex_err(SF_OPEN_FAILURE);
     sf_buffer out = sf_buffer_fixed(size);
+    sf_buffer_seek(&out, SF_BUFFER_END, 0);
 
     // Read into buffer
     if (fread(out.ptr, size, 1, f) < 0) {
